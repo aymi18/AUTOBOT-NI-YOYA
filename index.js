85b0ee5989ca34d5aac6bf02feb2aac3b88fee28
yoya <162847989+aymi18@users.noreply.github.com>
2024-05-04 07:22:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -760,7 +760,7 @@ function createConfig() {
       admin: [],
       devMode: false,
       database: false,
-      restartTime: 15,
+      restartTime: 999999999,
     },
     fcaOption: {
       forceLogin: true,
